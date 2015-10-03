48ea267c124e697f44473f4dba1eb8deb17995aa
microcheapfx <fx@microcheap.info>
2015-10-03 14:29:39 +0200

    Basic python package requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(\
+        name = 'pybale',\
+        version = '0.01',\
+        description = 'Bluetooth BLE module for python',\
+        url = '',\
+        author = '',\
+        author_email = '',\
+        license = '',\
+        packages = ['pybale'],\
+        zip_safe = False,\
+        )
